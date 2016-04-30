6aa1203681b4553d9e57ab17358cc6723e93a3d6
Jack Stanek <stanekjack@gmail.com>
2016-04-30 13:21:47 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fastacat',
-      version='0.0.3',
+      version='0.0.4',
       description='Concatenates FASTA genetic data between exons',
       author='Jack Stanek',
       author_email='stane064@umn.edu',
